6bbce31cb054b2ac136b00889348c34a6d0c979c
natasia noble <natasia.noble@gmail.com>
2023-12-06 13:22:03 +0100

    added new colors and new parties

--- interface/params.py
+++ interface/params.py
@@ -6,8 +6,18 @@ url = "interface/data/uk-constituencies-2019-BBC.hexjson"
 api_url = "https://ukelection-image-ne4yelgixa-no.a.run.app/predict"
 extra_cols = "https://storage.googleapis.com/uk_election_model/Scotland_NI_results.csv"
 
-parties = ["con", "lab", "lib", "oth"]
-party_colours = ["blue", "red", "orange", "lightgrey"]
+parties = ["con", "lab", "lib", "oth", "snp", "dup", "sf", "sdlp", "alliance"]
+party_colours = [
+    "#0087DC",
+    "#dc143c",
+    "#FAA61A",
+    "#005B54",
+    "#FDF38E",
+    "#D46A4C",
+    "#326760",
+    "#2AA82C",
+    "#F6CB2F",
+]
 colours_obj = alt.Color(
     "winning_party:N", scale=alt.Scale(domain=parties, range=party_colours)
 )
